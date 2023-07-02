cb567bc656eecc026c17edcfb4070c78983f1465
Nehatkr <nehathakur2308@gmail.com>
2023-07-02 19:17:41 +0530

    day42

---
+++ basics.cpp/FunctionTemplate.cpp
@@ -0,0 +1,32 @@
+// function template
+#include<iostream>
+using namespace std;
+// float funcAverage(int a, int b){
+//     float avg = (a+b)/2;
+//     return avg;
+// }
+// float funcAverage(int a, int b){
+//     float avg = (a+b)/2;
+//     return avg;
+// }
+template<class T>
+void swapp(T &a,T &b){
+    T temp = a;
+    a = b;
+    b = temp;
+}
+template <class T1 , class T2>
+float funcAverage(T1 a, T2 b){
+    float avg = (a+b)/2.0;
+   
+    return avg;
+}
+int main(){
+    float a;
+    a = funcAverage(5,2);
+    cout<<("The avegage of these number is %.3f",a)<<endl;
+     int x=5 ,y = 7;
+     swapp(x,y);
+     cout<<x<<endl<<y;
+    return 0;
+}
