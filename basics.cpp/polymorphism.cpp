c3a32bce04ab64193b41c8bd15bcc183039a6c3e
Nehatkr <nehathakur2308@gmail.com>
2023-07-06 09:23:01 +0530

    day36

---
+++ basics.cpp/polymorphism.cpp
@@ -0,0 +1,22 @@
+// polymorphism - One name and multiple forms
+// ex - function overloading , operators overloading , virtual functions
+
+/*
+polymorphism in c++ can be of to type:
+
+1. compile time polymorphism 
+   compile time polymorphism in c++ is acheived using :
+   1.1 - Function overloading
+   1.2 - Operator overloading
+
+2. Run time polymorphism 
+   Run time polymorphism in c++ is acheived using:
+   2.1 - Virtual function
+*/
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
