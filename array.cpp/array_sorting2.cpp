f3237883eb81aaa22d1363ddf8da85ad68032812
Nehatkr <nehathakur2308@gmail.com>
2023-08-07 08:30:20 +0530

    day49

---
+++ array.cpp/array_sorting2.cpp
@@ -0,0 +1,57 @@
+// sort an array consisting of only 0s and 1s.
+// before sorting {1,1,0,0,1,0,1,0}
+// after sorting {0,0,0,0,1,1,1,1}
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void sortZeroesAndOnes(vector<int> &v){
+    int left_ptr=0;
+    int right_ptr=v.size()-1;
+
+    while (left_ptr<right_ptr)
+    {
+       if (v[left_ptr]==1 && v[right_ptr]==0)
+       {
+        v[left_ptr]=0;
+        left_ptr++;
+        v[right_ptr]=1;
+        right_ptr--;
+       }
+      if (v[left_ptr]==0)
+      {
+       left_ptr++;
+      }
+      if (v[right_ptr]==1)
+      {
+        right_ptr--;
+      }
+      
+       
+    }
+    return;
+    
+}
+int main(){
+    int n;
+    cout<<"Enter the size of array";
+    cin>>n;
+
+    vector<int> v;
+    for (int  i = 0; i < n; i++)
+    {
+        int ele;
+        cin>>ele;
+        v.push_back(ele);
+    }
+    
+    sortZeroesAndOnes(v);
+    for (int i = 0; i < n; i++)
+    {
+        cout<<v[i]<<" ";
+    }cout<<endl;
+    
+
+    return 0;
+}
