8c1fed5b7219b7d0fa2681622076d279fa0dd855
Nehatkr <nehathakur2308@gmail.com>
2024-07-18 19:12:04 +0530

    prep insta

---
+++ prepInsta100Ques/negative_positiveNo.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the number: "<<endl;
+    cin>>n;
+    if(n>=0){
+        cout<<"Entered number is a positive number"<<endl;
+    }else{
+        cout<<"Entered number is a negative number"<<endl;
+    }
+
+    return 0;
+}
