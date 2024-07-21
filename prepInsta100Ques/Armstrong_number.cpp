977450f7185d635ae60e2ae605f5a016050304b8
Nehatkr <nehathakur2308@gmail.com>
2024-07-21 13:13:25 +0530

    practice

---
+++ prepInsta100Ques/Armstrong_number.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int num, count = 0, temp1;;
+    int digit,temp2,sum=0;
+
+    cout << "Enter the number: " << endl;
+    cin >> num;
+
+    temp1 = num;
+
+    while(temp1){
+        count++;
+        temp1=temp1/10;
+    }
+
+    temp2=num;
+
+    while (temp2 != 0)
+    {
+        
+        digit = temp2 % 10;
+       sum = sum + pow(digit, count);
+        temp2 = temp2 / 10;
+    }
+
+    cout<<"The sum is: "<<sum<<endl;
+
+    if (num == sum)
+    {
+        cout << num << "  is Armstrong number"<<endl;
+    } else
+    {
+        cout << num << "  not a Armstrong number";
+    }
+
+    return 0;
+}
