22e23dcfaff954064e35b8e3e4e92cb218bd40d6
Nehatkr <nehathakur2308@gmail.com>
2024-07-23 23:00:41 +0530

    practice

---
+++ prepInsta100Ques/factors_of_number.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout<<"Enter the number"<<endl;
+    cin>>num;
+    cout<<"Factor of num "<<num<<" are:"<<endl;
+    for (int i = 1; i <= num; i++)
+    {
+        if(num%i==0){
+            cout<<i<<" ";
+        }
+    }
+    
+    return 0;
+
+}
