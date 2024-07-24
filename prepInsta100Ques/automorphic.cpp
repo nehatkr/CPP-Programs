91bac514fd6782781c276c1dfb040a0434e39897
Nehatkr <nehathakur2308@gmail.com>
2024-07-24 18:24:31 +0530

    practice

---
+++ prepInsta100Ques/automorphic.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int isAutomorphic(int n){
+    
+    int square = n * n;
+    
+    while(n != 0)
+    {
+        // means not automorphic number
+        if(n % 10 != square % 10){
+            return 0;
+        }
+        
+        // reduce down numbers
+        n /= 10;
+        square /= 10;
+    }
+    // if reaches here means automorphic number
+    return 1;
+}
+
+int main ()
+{
+    int n=376 , sq =n*n;
+    
+    // cout<<"Enter the number to check wether the given number is Automorphic or not:"<<endl;
+    // cin>>n;
+
+    if(isAutomorphic(n))
+        cout << "Num: "<< n << ", Square: " << sq << " - is Automorphic";
+    else
+        cout << "Num: "<< n << ", Square: " << sq << " - is not Automorphic";
+    
+
+}
+// Time complexity: O(N)
+// Space complexity: O(1)
