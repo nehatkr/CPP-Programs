0d333ee0436e361fd73ba0957edfc9a3000e707b
Nehatkr <nehathakur2308@gmail.com>
2024-08-27 21:13:03 +0530

    practice

--- prepInsta100Ques/QuestionRelated_To_Array.cpp/non-repeatingElement.cpp
+++ prepInsta100Ques/QuestionRelated_To_Array.cpp/non-repeatingElement.cpp
@@ -1,30 +1,28 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    int arr[] = {2, 7, 2, 4, 4, 2, 3, 6, 1, 6};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int visited[n], count = 0;
+// Main function to run the program
+int main() 
+{ 
+    int arr[] = {10, 30, 10, 20, 40, 20, 50, 10}; 
+    int n = sizeof(arr)/sizeof(arr[0]); 
 
-    for (int i = 0; i < n; i++)
-    {
-        if (visited[i] != 1)
-        {
-            for (int j = i + 1; j < n; j++)
-            {
-                if (arr[i] == arr[j])
-                {
-                    count++;
-                    visited[j]=1;
-                }
-            }
-            if (count == 1)
-            {
-                cout << arr[i] << " ";
-            }
-        }
-    }
+    int visited[n], count_dis=0;
 
-    return 0;
+    for(int i=0; i<n; i++){
+
+        if(visited[i]!=1){
+           int count = 1;
+           for(int j=i+1; j<n; j++){
+              if(arr[i]==arr[j]){
+                 count++;
+                 visited[j]=1;
+              }
+            }
+            if(count==1)
+             cout<<arr[i]<<" ";
+         }
+     }
+ 
+    return 0; 
 }
