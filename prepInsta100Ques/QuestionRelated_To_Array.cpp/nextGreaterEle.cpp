58c3b96eaabaffb08034657de3b4a70f7d8dee0b
Nehatkr <nehathakur2308@gmail.com>
2024-11-11 20:37:54 +0530

    practice

---
+++ prepInsta100Ques/QuestionRelated_To_Array.cpp/nextGreaterEle.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+void nextGreaterEle(int n,int arr[]){
+    int i,j,next;
+    for ( i = 0; i < n; i++)
+    {
+       next=-1;
+       for ( j = i+1; j <n ; j++)
+       {
+        if(arr[i]<arr[j]){
+            next=arr[j];
+            break;
+        }
+       }
+       cout<<arr[i]<<"-->"<<next<<endl;
+    }
+    
+}
+
+int main(){
+int arr[]={4,5,2,25};
+int n=sizeof(arr)/sizeof(arr[0]);
+
+nextGreaterEle(n,arr);
+ return 0;
+}
