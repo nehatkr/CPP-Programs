dea925452fa7653886c570f48e27a381a43cd8b3
Nehatkr <nehathakur2308@gmail.com>
2024-11-25 22:19:20 +0530

    practice

---
+++ prepInsta100Ques/accentureCodingQue/evenIntegerSum.cpp
@@ -0,0 +1,19 @@
+// return the sum of all even numbered in killometer till n they complete
+// input:10
+// output:30
+// exp: 2+4+6+8+10=30
+#include<iostream>
+using namespace std;
+
+int evenSum(int n){
+    int count=0;
+    for (int i = 1; i <= n; i++)
+    {
+       if(1%2==0){
+        count = count + i;
+       }
+    }
+    return count;
+
+}
+
