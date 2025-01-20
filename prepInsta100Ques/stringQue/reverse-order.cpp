b423eb45d15e6e5211d81a27b8df2396a71823db
Nehatkr <nehathakur2308@gmail.com>
2025-01-20 21:42:41 +0530

    practice

---
+++ prepInsta100Ques/stringQue/reverse-order.cpp
@@ -0,0 +1,29 @@
+// C++ Program to reverse a string using two 
+// pointer method 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s = "Hello World";
+
+    // Initialize two pointers: left at start
+    //  and right at the end of the string
+    int l = 0;
+    int r = s.length() - 1;
+
+    // Loop until the two pointers meet in the middle
+    while (l < r) {
+      
+        // Swap characters at position left and right
+        swap(s[l], s[r]);
+
+        // Move the left pointer to right
+        l++;
+
+        // Move the right pointer to left
+        r--;
+    }
+
+    cout << s;
+    return 0;
+}
