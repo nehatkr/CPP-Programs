99472cc08ec2a847c26276fb876e1b2723f72704
Nehatkr <nehathakur2308@gmail.com>
2025-03-26 18:25:00 +0530

    practice

--- prepInsta100Ques/accentureCodingQue/decryptString.cpp
+++ prepInsta100Ques/accentureCodingQue/decryptString.cpp
@@ -6,10 +6,10 @@ using namespace std;
 int helper(string str, int n){
    int sum = 0;
     string ans = "";
-    for (int i = 1; i <= str.length(); i+2) //skip one index on every iteration
+    for (int i = 1; i <= str.length(); i+2) //skip one index on every iteration (2,1 is a string here)
     {
-        sum = sum + str[i]-'0';
-        if (sum>=n) //check if given position is equel to sum then ans
+         sum = sum + str[i]-'0';  //converts the character at position i to a numeric digit.
+        if (sum>=n) //check if given position is equal to sum then ans
         {
            ans = ans+str[i-1];
         //    return ans;
